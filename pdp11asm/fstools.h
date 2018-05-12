b5d161d52c145ae01cd74ea64974c26e5f4d75fe
Ivanq <imachug@yandex.ru>
2018-05-12 10:35:46 +0300

    Optimization

--- pdp11asm/fstools.h
+++ pdp11asm/fstools.h
@@ -1,22 +1,10 @@
 #include <string>
 
-#ifdef WIN32
-void saveStringToFile(const char* fileName, const void* buf, size_t len);
-void loadStringFromFile(std::string& buf, const char* fileName);
-void chdirToFile(const char* fileName);
-std::string replaceExtension(const std::string& fileName, const char* ext);
-
-typedef char syschar_t;
-typedef std::string sysstring_t;
-#define _T(S) L##S
-#else
 typedef char syschar_t;
 typedef std::string sysstring_t;
 #define _T(S) S
-#endif
 
 void saveStringToFile(const char* fileName, const void* buf, size_t len);
 void loadStringFromFile(std::string& buf, const char* fileName);
 void chdirToFile(const char* fileName);
 std::string replaceExtension(const std::string& fileName, const char* ext);
-
